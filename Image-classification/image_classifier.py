69dce361754bdf54f13c2f2fe6eae3e699cedbbb
Nathaniel Mitrani <104577523+NathanielMH@users.noreply.github.com>
2022-06-23 01:55:24 +0200

    Update image_classifier.py

--- Image-classification/image_classifier.py
+++ Image-classification/image_classifier.py
@@ -1,1 +1,74 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.utils import to_categorical
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+%matplotlib inline
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# One hot encoding
+
+y_train_encoded = to_categorical(y_train)
+y_test_encoded = to_categorical(y_test)
+
+# Reshape the data for input in NN
+
+x_train_reshaped = np.reshape(x_train, (60000, 784))
+x_test_reshaped = np.reshape(x_test, (10000, 784))
+
+# Normalize data
+x_mean = np.mean(x_train_reshaped)
+x_std = np.std(x_train_reshaped)
+
+epsilon = 1e-10
+x_train_norm = (x_train_reshaped - x_mean)/(x_std + epsilon)
+x_test_norm = (x_test_reshaped - x_mean)/(x_std + epsilon)
+
+# Creating the model
+model = Sequential([
+    Dense(128, activation = 'relu', input_shape = (784,)),
+    Dense(128, activation = 'relu'),
+    Dense(10, activation = 'softmax')
+])
+
+# Compile the model
+model.compile(
+    optimizer = 'sgd',
+    loss = 'categorical_crossentropy',
+    metrics = ['accuracy']
+)
+
+# Train the model
+h = model.fit(
+    x_train_norm,
+    y_train_encoded,
+    epochs = 3
+)
+
+# Model evaluation
+loss, accuracy = model.evaluate(x_test_norm, y_test_encoded)
+
+# Model predictions
+preds = model.predict(x_test_norm)
+
+# Plot model predictions
+plt.figure(figsize = (12, 12))
+
+start_index = 0
+
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    pred = np.argmax(preds[start_index + i])
+    actual = np.argmax(y_test_encoded[start_index + i])
+    col = 'g'
+    if pred != actual:
+        col = 'r'
+    plt.xlabel('i={} | pred={} | true={}'.format(start_index + i, pred, actual), color = col)
+    plt.imshow(x_test[start_index + i], cmap='binary')
+plt.show()
