815811e7b8475feafe9aa1f977cf1d7cccd0a490
Nathaniel Mitrani <104577523+NathanielMH@users.noreply.github.com>
2022-06-24 00:58:32 +0200

    Update facial_expression_classifier.py

--- Facial-expression-classification/facial_expression_classifier.py
+++ Facial-expression-classification/facial_expression_classifier.py
@@ -1,1 +1,208 @@
+import tensorflow as tf
+import cv2
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import tensorflow as tf
+import pickle
+from tensorflow.python.keras import *
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, optimizers
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.utils import plot_model
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
+from IPython.display import display
+from tensorflow.keras import backend as K
 
+# Read data
+emotion_df = pd.read_csv('emotion.csv')
+
+# Converting pixels to arrays
+def string2array(x):
+  return np.array(x.split(' ')).reshape(48, 48, 1).astype('float32')
+
+emotion_df['pixels'] = emotion_df['pixels'].apply(lambda x: string2array(x))
+
+# Checking for the presence of null values in the data frame
+emotion_df.isnull().sum()
+
+# Data augmentation and preparation to train the model
+
+# Split the dataframe to features and labels
+# from keras.utils import to_categorical
+
+X = emotion_df['pixels']
+# y = to_categorical(emotion_df['emotion'])
+y = pd.get_dummies(emotion_df['emotion'])
+
+X = np.stack(X, axis = 0)
+X = X.reshape(24568, 48, 48, 1)
+
+# Converting data into arrays
+X = np.array(X)
+y = np.array(y)
+
+# Separate the data to train, test and validate
+X_train, X_Test, y_train, y_Test = train_test_split(X,y,test_size = 0.1, shuffle = True)
+X_val, X_Test, y_val, y_Test = train_test_split(X_Test,y_Test, test_size = 0.5, shuffle = True)
+
+# Image pre-processing
+X_train = X_train/255
+X_val   = X_val /255
+X_Test  = X_Test/255
+
+# Data augmentation
+train_datagen = ImageDataGenerator(
+rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
+    fill_mode = "nearest"
+)
+
+# Build RES block from our CNN
+def res_block(X, filter, stage):
+
+  # Convolutional_block
+  X_copy = X
+
+  f1 , f2, f3 = filter
+
+  # Main Path
+  X = Conv2D(f1, (1,1),strides = (1,1), name ='res_'+str(stage)+'_conv_a', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = MaxPool2D((2,2))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_a')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f2, kernel_size = (3,3), strides =(1,1), padding = 'same', name ='res_'+str(stage)+'_conv_b', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_b')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_conv_c', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_c')(X)
+
+
+  # Short path
+  X_copy = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_conv_copy', kernel_initializer= glorot_uniform(seed = 0))(X_copy)
+  X_copy = MaxPool2D((2,2))(X_copy)
+  X_copy = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_copy')(X_copy)
+
+  # ADD
+  X = Add()([X,X_copy])
+  X = Activation('relu')(X)
+
+  # Identity Block 1
+  X_copy = X
+
+
+  # Main Path
+  X = Conv2D(f1, (1,1),strides = (1,1), name ='res_'+str(stage)+'_identity_1_a', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_1_a')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f2, kernel_size = (3,3), strides =(1,1), padding = 'same', name ='res_'+str(stage)+'_identity_1_b', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_1_b')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_identity_1_c', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_1_c')(X)
+
+  # ADD
+  X = Add()([X,X_copy])
+  X = Activation('relu')(X)
+
+  # Identity Block 2
+  X_copy = X
+
+
+  # Main Path
+  X = Conv2D(f1, (1,1),strides = (1,1), name ='res_'+str(stage)+'_identity_2_a', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_2_a')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f2, kernel_size = (3,3), strides =(1,1), padding = 'same', name ='res_'+str(stage)+'_identity_2_b', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_2_b')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_identity_2_c', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_2_c')(X)
+
+  # ADD
+  X = Add()([X,X_copy])
+  X = Activation('relu')(X)
+
+  return X
+
+# Building the model
+input_shape = (48, 48, 1)
+
+# Input tensor shape
+X_input = Input(input_shape)
+
+# Zero-padding
+X = ZeroPadding2D((3, 3))(X_input)
+
+# 1 - stage
+X = Conv2D(64, (7, 7), strides= (2, 2), name = 'conv1', kernel_initializer= glorot_uniform(seed = 0))(X)
+X = BatchNormalization(axis =3, name = 'bn_conv1')(X)
+X = Activation('relu')(X)
+X = MaxPooling2D((3, 3), strides= (2, 2))(X)
+
+# 2 - stage
+X = res_block(X, filter= [64, 64, 256], stage= 2)
+
+# 3 - stage
+X = res_block(X, filter= [128, 128, 512], stage= 3)
+
+# 4 - stage
+# X = res_block(X, filter= [256, 256, 1024], stage= 4)
+
+# Average Pooling
+X = AveragePooling2D((2, 2), name = 'Averagea_Pooling')(X)
+
+# Final layer
+X = Flatten()(X)
+X = Dense(5, activation = 'softmax', name = 'Dense_final', kernel_initializer= glorot_uniform(seed=0))(X)
+
+model_emotion = Model( inputs= X_input, outputs = X, name = 'Resnet18')
+
+# Set network parameters
+model_emotion.compile(optimizer = "Adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
+
+# Using early stopping to exit training if validation loss is not decreasing even after certain epochs (patience)
+earlystopping = EarlyStopping(monitor = 'val_loss', mode = 'min', verbose = 1, patience = 20)
+
+# Save the best model with lower validation loss
+checkpointer = ModelCheckpoint(filepath = "FacialExpression_weights.hdf5", verbose = 1, save_best_only=True)
+
+# Train model
+history = model_emotion.fit(train_datagen.flow(X_train, y_train, batch_size=64),
+	validation_data=(X_val, y_val), steps_per_epoch=len(X_train) // 64,
+	epochs= 20, callbacks=[checkpointer, earlystopping])
+
+# Saving the model architecture to json file for future use
+model_json = model_emotion.to_json()
+with open("Emotion-model.json","w") as json_file:
+  json_file.write(model_json)
+  
+  with open('Emotion-model.json', 'r') as json_file:
+    json_savedModel= json_file.read()
+    
+# Load the model architecture 
+model_emotion = tf.keras.models.model_from_json(json_savedModel)
+model_emotion.load_weights('FacialExpression_weights.hdf5')
+model_emotion.compile(optimizer = "Adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
+
+# Evaluate the model
+score = model_emotion.evaluate(X_Test, y_Test)
+print('Test Accuracy: {}'.format(score[1]))
